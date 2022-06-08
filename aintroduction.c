5f89fb8b2817238a4985fb9f456c038919be79e4
Pankaj Dukare <88884284+pankajdukare73@users.noreply.github.com>
2022-06-08 16:06:47 +0530

    Add files via upload

---
+++ aintroduction.c
@@ -0,0 +1,18 @@
+/*Author:Pankaj Dukare
+------------------------------C Programming Introduction------------------------------------
+    -C is a powerful general-purpose programming language.
+    -Develope by Dennis Ritchie in 1970 at the Bell Telephone Laboratories.
+    -It can be used to develop software like operating systems, databases, compilers, and so on.
+    -C programming is an excellent language to learn to program for beginners.
+*/
+
+#include <stdio.h>
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+   return 0;
+}
+/*
+Output:
+    Hello, World! 
+*/
